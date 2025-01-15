82cad2a5847ba3328615a39baf2383eadc3086d9
Romário Vieira <romario.sistemas@gmail.com>
2025-01-15 00:12:54 -0300

    Configuração projeto local
    Criação projeto

---
+++ src/test/java/com/roma/prod/RomaSrvCadastroProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roma.prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaSrvCadastroProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
